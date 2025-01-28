7226b0f5754b95cbc466dacbfc3b9f06095eb8a1
prashantmestry <mestry.prashant10@gmail.com>
2025-01-28 18:41:50 +0530

    fix: Commit message guidelin

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 72],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
+    ],
+    "subject-case": [2, "always", "sentence-case"], // Example: Ensure the subject is sentence-case
+  },
+  parserPreset: {
+    parserOpts: {
+      noteKeywords: ["BREAKING CHANGE", "RELEASE NOTE"],
+    },
+  },
 };
