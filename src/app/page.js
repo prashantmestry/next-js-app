c83c57959d118b16253695ce98676eddecb14dfb
prashantmestry <mestry.prashant10@gmail.com>
2025-01-28 19:30:04 +0530

    fix: Axios added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
-import Image from "next/image";
-import Footer from "./component/Footer";
-import Header from "./component/Header";
+"use client";
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log("error", err));
+  }, []);
+
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      This is main page , I am working
+    <main>
+      <div className="flex flex-col">
+        {data.map((item) => {
+          return <div key={item.id}>{item.name}</div>;
+        })}
+      </div>
     </main>
   );
 }
